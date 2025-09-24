8d9fafc91b92ce8352bfacdfca713764c911dcc3
Tomás Migone <tomas@edgeandnode.com>
2025-09-24 16:42:29 -0300

    nice config command
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- src/commands/config.ts
+++ src/commands/config.ts
@@ -1,6 +1,7 @@
 import * as Command from "@effect/cli/Command"
 import { Console, Effect } from "effect"
 import { ConfigService } from "../services/ConfigService.js"
+import { Display } from "../utils/Display.js"
 
 export const config = Command.make("config", {
   args: {}
@@ -9,13 +10,10 @@ export const config = Command.make("config", {
   Command.withHandler(() =>
     Effect.gen(function*() {
       const config = yield* ConfigService
-      yield* Console.log("Configuration:")
-      yield* Console.log("===================")
+      yield* Display.section("Configuration")
       yield* Console.log(`RPC URL: ${config.rpcUrl}`)
       yield* Console.log(`Subgraph URL: ${config.subgraphUrl}`)
       yield* Console.log(`Studio API Key: ${config.studioApiKey}`)
-      yield* Console.log(`Output Format: ${config.outputFormat}`)
-      yield* Console.log(`Show Discrepancies: ${config.showDiscrepancies}`)
     })
   )
 )
